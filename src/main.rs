7840c9376ac59696e56b19c359d430cbd939b892
Wellington Garcia <garciawell@gmail.com>
2023-03-30 22:52:34 -0300

    Understand scope

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+
+    // VARIABLE
     println!("Hello, world!");
 
     let number = 16;
@@ -7,4 +9,23 @@ fn main() {
     cars = 34;
 
     const CAR_TEST: f64 = 9.8;
+
+
+    // SCOPES
+    let x = 5;
+    {
+        let y = 99;
+        println!("{}, {}", x, y)
+    }
+    println!("{}, {}", x, y) // ERROR
+
+    //SCOPE 2 
+    let x = 5;
+    {
+        let x = 99;
+        println!("{}", x) // Prints 99
+    }
+    println!("{}", x) // Prints 5
+
+
 }
