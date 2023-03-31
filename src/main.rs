3ab8823e46d805f48e402c01cbf24521483fe997
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-03-30 23:09:04 -0300

    exercise part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    let missiles: i32 = 8;
-    let ready: i32 = 32;
+    let  mut missiles: i32 = STARTING_MISSILES;
+    let ready: i32 = READY_AMOUNT;
+
+    missiles = missiles - ready;
+
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles);
 }
