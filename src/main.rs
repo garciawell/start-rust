5a041cb4d4b3d13828762819f9733668d3829b30
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-03-30 23:04:14 -0300

    exercise part 1

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,6 @@
 fn main() {
+    let missiles: i32 = 8;
+    let ready: i32 = 32;
 
-    // VARIABLE
-    println!("Hello, world!");
-
-    let number = 16;
-
-    let mut cars = 2;
-    cars = 34;
-
-    const CAR_TEST: f64 = 9.8;
-
-
-    // SCOPES
-    let x = 5;
-    {
-        let y = 99;
-        println!("{}, {}", x, y)
-    }
-    println!("{}, {}", x, y) // ERROR
-
-    //SCOPE 2 
-    let x = 5;
-    {
-        let x = 99;
-        println!("{}", x) // Prints 99
-    }
-    println!("{}", x) // Prints 5
-
-
-    // MEMORY SAFETY
-    let enigma: i32;
-    if true {
-        enigma = 42;
-    }
-    println!("{}", enigma) // ERROR you must to initialize variable
-
-    ///////
-    let enigma: i32;
-    if true {
-        enigma = 42;
-    } else {
-        enigma = 12; 
-    }
-    println!("{}", enigma) // WORKED
-
+    println!("Firing {} of my {} missiles...", ready, missiles);
 }
