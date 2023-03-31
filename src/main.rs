fdf4b50215db55f7b0126210179d6c67034785df
Wellington Garcia <wellington.garcia@bancointer.com.br>
2023-03-30 23:20:03 -0300

    resolve exercise 2

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,44 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_variables)]
+
 fn main() {
-
-    // VARIABLE
-    println!("Hello, world!");
-
-
-
-    let mut cars = 2;
-    cars = 34;
-
-    const CAR_TEST: f64 = 9.8;
-
-
-    // SCOPES
-    let x = 5;
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+    // 1. Try running this code with `cargo run` and take a look at the error.
+    //
+    // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
+    // doing `cargo run` should succeed and print something out.
     {
-        let y = 99;
-        println!("{}, {}", x, y)
+        let area = area_of(width, height);
+        println!("Area is {}", area);
     }
 
+    // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
+    //    the code again and make sure it worked (you should get an area of 28).
 
-    //SCOPE 2 
-    let x = 5;
-    {
-        let x = 99;
-        println!("{}", x) // Prints 99
-    }
-
-    // MEMORY SAFETY
-    let enigma: i32;
-    if true {
-        enigma = 42;
-    }
-
-    ///////
-    let enigma: i32;
-    if true {
-        enigma = 42;
-    } else {
-        enigma = 12; 
-    }
-    println!("{}", enigma);
-
-   let result =  do_stuff(2.3,3.4);
-
-    println!("RESULT {}", result); 
+    // 3. Uncomment the line below.  It doesn't work yet because the `volume` function doesn't exist.
+    //    Create the `volume` function!  It should:
+    //    - Take three arguments of type i32
+    //    - Multiply the three arguments together
+    //    - Return the result (which should be 280 when you run the program).
+    //
+    // If you get stuck, remember that this is *very* similar to what `area_of` does.
+    //
+    println!("Volume is {}", volume(width, height, depth));
 }
 
+fn area_of(x: i32, y: i32) -> i32 {
+    // 2a. Fix this function to correctly compute the area of a rectangle given
+    // dimensions x and y by multiplying x and y and returning the result.
+    //
+    return x * y;
+    // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
+    //            `return` on the last line of a function. Change the last line to be a
+    //            "tail expression" that returns a value without using `return`.
+    //            Hint: `cargo clippy` will warn you about this exact thing.
+}
 
-fn do_stuff(qty: f64, oz: f64) -> f64{
-     qty * oz
+fn volume(w: i32, h:i32, d:i32) -> i32 {
+   w * h * d
 }
