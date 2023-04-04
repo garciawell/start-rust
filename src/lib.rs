d1b2a66b75b4850f87d01c4caa1c43b3765437ca
Wellington Garcia <garciawell@gmail.com>
2023-04-04 00:10:38 -0300

    Traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
 pub fn greet() {
-    println!("Hi!")
+    println!("Hi!");
+
+    print_noise(23);
 }
+
+
+struct RedFox {
+    enemy: bool,
+    life: u32
+}
+
+trait Noisy {
+    fn get_noise(&self) -> &str;
+}
+
+impl Noisy for RedFox {
+   fn get_noise(&self) -> &str { "Ua Uau!" }
+}
+
+impl Noisy for u8 {
+    fn get_noise(&self) -> &str { "BYTE" }
+ }
+
+ fn print_noise<T: Noisy>(item: T) {
+    println!("{}", item.get_noise());
+ }
