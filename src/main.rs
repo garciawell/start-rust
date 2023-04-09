0452b8145f0c5c4150f09e6f71f211bbcb7d1072
Wellington Garcia <garciawell@gmail.com>
2023-04-09 18:05:00 -0300

    Create Frame and render

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::error::Error;
+use std::{error::Error, io, time::Duration, sync::mpsc, thread};
+use crossterm::{terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand, cursor::{Hide, Show}, event::{self, KeyCode, Event}};
+use invaders::{frame::{self, new_frame}, render};
 use rusty_audio::Audio;
 
 fn main()  -> Result<(), Box<dyn Error>> {
@@ -12,7 +14,55 @@ fn main()  -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+    let (render_tx,  render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+           let curr_frame =  match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game Loop
+    'gameLoop: loop {
+        // Per -frame init
+        let curr_frame = new_frame();
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(ket_event) = event::read()? {
+                match ket_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameLoop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
