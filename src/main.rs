05056ba954504148043f3df7d330fe2c2f92605f
Wellington Garcia <garciawell@gmail.com>
2023-04-10 22:37:07 -0300

    final commit invaders

--- src/main.rs
+++ src/main.rs
@@ -70,6 +70,9 @@ fn main()  -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & render
         player.draw(&mut curr_frame);
@@ -80,6 +83,16 @@ fn main()  -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or Lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameLoop;
+        }
+
+        if invaders.reached_bottom() {
+            audio.play("lose");
+        }
     }
 
     // Cleanup
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,10 @@
+use crate::{
+    frame::{Drawable, Frame},
+    invaders::Invaders,
+    shot::Shot,
+    {NUM_COLS, NUM_ROWS},
+};
 use std::time::Duration;
-
-use crate::{NUM_COLS, NUM_ROWS, frame::{Frame, Drawable}, shot::Shot};
 
 pub struct Player {
     x: usize,
@@ -10,8 +14,8 @@ pub struct Player {
 
 impl Player {
     pub fn new() -> Self {
-        Self  {
-            x: NUM_COLS /2,
+        Self {
+            x: NUM_COLS / 2,
             y: NUM_ROWS - 1,
             shots: Vec::new(),
         }
@@ -36,19 +40,35 @@ impl Player {
     }
     pub fn update(&mut self, delta: Duration) {
         for shot in self.shots.iter_mut() {
-            shot.update(delta)
+            shot.update(delta);
         }
-
         self.shots.retain(|shot| !shot.dead());
+    }
+    pub fn detect_hits(&mut self, invaders: &mut Invaders) -> bool {
+        let mut hit_something = false;
+        for shot in self.shots.iter_mut() {
+            if !shot.exploding {
+                if invaders.kill_invader_at(shot.x, shot.y){
+                    hit_something = true;
+                    shot.explode();
+                }
+            }
+        }
+        hit_something
     }
 }
 
+impl Default for Player {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl Drawable for Player {
     fn draw(&self, frame: &mut Frame) {
         frame[self.x][self.y] = "A";
         for shot in self.shots.iter() {
             shot.draw(frame);
-        } 
+        }
     }
 }
